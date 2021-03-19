a6a6350a17f2e5a280c2b18c2ad96ca0869c6a14
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-03-19 13:42:05 +0900

    rust 001 dispfile

---
+++ 001/dispfile.rs
@@ -0,0 +1,21 @@
+//dispfile.rs
+use std::env;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+  let argv: Vec<String> = env::args().collect();
+  if argv.len() < 2 {
+    println!("Let dist filename on command arguments");
+    std::process::exit(1);
+  }
+  
+  let f = File::open(&argv[1])?;
+  let reader = BufReader::new(f);
+
+  for line in reader.lines() {
+    println!("{}", line?);
+  }
+  Ok(())
+}
