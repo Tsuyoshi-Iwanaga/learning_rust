b021ed1309d2310b79e54b142a56c47956f1a92f
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-03-24 21:09:34 +0900

    remove twice.rs

--- 003/twice.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-  println!("input integer!");
-  let mut s = String::new();
-  std::io::stdin().read_line(&mut s).ok();
-  let n:i32 = s.trim().parse().ok().unwrap();
-
-  println!("twice of {} is {}", n, n*2);
-}
