2bea847d94c0a7a78e5d88fcc875c6756aa32871
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-04-02 00:17:42 +0900

    rust iterate while loop

--- 004/iterate.rs
+++ 004/iterate.rs
@@ -15,4 +15,41 @@ fn main() {
       println!("{} {}", i, j);
     }
   }
+
+  //continueはループを継続させる
+  for i in 0..10 {
+    if i % 2 == 0 {
+      continue
+    }
+    println!("{}は奇数", i);
+  }
+
+  //配列に対してfor文を使う
+  let fruits = ["りんご", "みかん", "バナナ"];
+  for x in fruits.iter() {
+    println!("{}", x);
+  }
+
+  //Vectorに対してfor文を使う
+  let fruits2 = vec!["りんご", "みかん", "バナナ"];
+  for x in fruits2.iter() {
+    println!("{}(Vector)", x);
+  }
+
+  //while
+  let mut count = 0;
+  while count < 10 {
+    println!("while:{}", count);
+    count = count + 1;
+  }
+
+  //loop
+  let mut l = 0;
+  loop {
+    println!("loop:{}", l);
+    l = l + 1;
+    if l > 5 {
+      break;
+    }
+  }
 }
