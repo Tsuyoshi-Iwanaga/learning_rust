a6ae435a9eadb7f4da53e847366561bd930e7a15
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-04-19 23:58:29 +0900

    remove rust file

--- 009/file02.rs
+++
@@ -1,6 +0,0 @@
-use std::fs;
-
-fn main() -> std::io::Result<()> {
-  fs::create_dir("./misc/subdir")?;
-  Ok(())
-}
--- 009/file03.rs
+++
@@ -1,7 +0,0 @@
-use std::fs;
-use std::path::PathBuf;
-
-fn main() {
-  let srcdir = PathBuf::from("./src");
-  println!("{:?}", fs::canonicalize(&srcdir));
-}
