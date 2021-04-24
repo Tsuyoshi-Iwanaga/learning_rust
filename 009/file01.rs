f7c3c4e6e0b13861ab049e01447a39740c6b8459
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-04-24 23:51:31 +0900

    update rust file01

---
+++ 009/file01.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+  let argv: Vec<String> = env::args().collect();
+  let argc = argv.len();
+  if argc < 3 {
+    println!("select 2 args");
+    std::process::exit(1);
+  }
+
+  let src = &argv[1];
+  let dest = &argv[2];
+  fs::copy(src, dest)?;
+  Ok(())
+}
